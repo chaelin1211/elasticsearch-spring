20c178463ac694a193e59a13a24894907f9c8d80
chaelin1211 <cofls749@naver.com>
2024-04-15 17:26:48 +0900

    FEAT: Exception 처리 추가

--- src/main/java/com/demo/es/search/service/SearchService.java
+++ src/main/java/com/demo/es/search/service/SearchService.java
@@ -1,38 +1,49 @@
 package com.demo.es.search.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.HighlightField;
 import com.demo.es.search.dto.ProductResponse;
 import com.demo.es.search.dto.SearchLogResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SearchService {
     private final ElasticsearchClient elasticsearchClient;
 
-    public List<ProductResponse> getProducts(String searchWord) throws IOException {
-        SearchResponse<ProductResponse> searchResponse = elasticsearchClient.search(sb -> sb
-                        .index("products")
+    public List<ProductResponse> getProducts(String searchWord) {
+        SearchResponse<ProductResponse> searchResponse;
+
+        try {
+            searchResponse = elasticsearchClient.search(sb -> sb
+                            .index("products")
                         .query(qb -> qb.bool(bb -> bb
                                 .should(List.of(
                                     QueryBuilders.match(mq -> mq.field("name").query(searchWord)),
                                     QueryBuilders.match(mq -> mq.field("desc").query(searchWord)))
                                 )
-                        ))
-                        .highlight(hb -> hb.fields(new HashMap<>() {{
-                            put("name", HighlightField.of(hf -> hf.type("plain")));
-                            put("desc", HighlightField.of(hf -> hf.type("plain")));
-                        }}))
-                , ProductResponse.class);
+                            ))
+                            .highlight(hb -> hb.fields(new HashMap<>() {{
+                                put("name", HighlightField.of(hf -> hf.type("plain")));
+                                put("desc", HighlightField.of(hf -> hf.type("plain")));
+                            }}))
+                    , ProductResponse.class);
+        } catch (IOException | ElasticsearchException e) {
+            log.error(e.getMessage());
+            return Collections.emptyList();
+        }
 
         List<ProductResponse> productResponses = new ArrayList<>();
         searchResponse.hits().hits().forEach(hit -> {
@@ -48,14 +59,21 @@ public class SearchService {
         return productResponses;
     }
 
-    public List<SearchLogResponse> getSearchLogList(String searchWord) throws IOException {
-        SearchResponse<SearchLogResponse> searchResponse = elasticsearchClient.search(sb -> sb
-                        .index("search_log")
-                        .query(qb -> qb.match(mb -> mb.field("word").query(searchWord)))
-                        .highlight(hb -> hb.fields(new HashMap<>() {{
-                            put("word", HighlightField.of(hf -> hf.type("plain")));
-                        }}))
-                , SearchLogResponse.class);
+    public List<SearchLogResponse> getSearchLogList(String searchWord) {
+        SearchResponse<SearchLogResponse> searchResponse;
+
+        try {
+            searchResponse = elasticsearchClient.search(sb -> sb
+                            .index("search_log")
+                            .query(qb -> qb.match(mb -> mb.field("word").query(searchWord)))
+                            .highlight(hb -> hb.fields(new HashMap<>() {{
+                                put("word", HighlightField.of(hf -> hf.type("plain")));
+                            }}))
+                    , SearchLogResponse.class);
+        } catch (IOException | ElasticsearchException e) {
+            log.error(e.getMessage());
+            return Collections.emptyList();
+        }
 
         List<SearchLogResponse> searchLogResponses = new ArrayList<>();
         searchResponse.hits().hits().forEach(hit -> {
